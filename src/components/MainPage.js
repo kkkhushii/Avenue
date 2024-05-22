ba32479a42870b9c694b872744f44924efac2c8e
kkkhushii <khushijoshi512@gmail.com>
2024-05-22 12:37:53 +0530

    changes

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,6 +4,7 @@ import Heropage from './Hero/Heropage'
 import Destination from '../components/Destination/Destination'
 import Feature from '../components/Feature/Feature'
 import TourPackages from '../components/TourPackages/TourPackages'
+import PopularCountries from '../components/PopularContries/PopularCountries'
 
 function MainPage() {
   return (
@@ -13,7 +14,7 @@ function MainPage() {
       <Destination />
       <Feature />
       <TourPackages />
-
+      <PopularCountries />
     </div>
   )
 }
---
+++ src/components/PopularContries/PopularCountries.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+function PopularCountries() {
+
+
+    const countries = [
+        { name: 'France', offers: '1500 Offers', imgSrc: '/Icons/france 1.svg' },
+        { name: 'Spain', offers: '135 Offers', imgSrc: '/Icons/spain 1.svg' },
+        { name: 'China', offers: '1593 Offers', imgSrc: '/Icons/china 1.svg' },
+        { name: 'Turkey', offers: '69 Offers', imgSrc: '/Icons/turkey 1.svg' },
+        { name: 'Germany', offers: '589 Offers', imgSrc: '/Icons/germany 1.svg' },
+        { name: 'Thailand', offers: '3600 Offers', imgSrc: '/Icons/thailand 1.svg' },
+        { name: 'Maxico', offers: '330 Offers', imgSrc: '/Icons/mexico 1.svg' },
+        { name: 'Canada', offers: '1030 Offers', imgSrc: '/Icons/canada 1.svg' },
+        { name: 'Brazil', offers: '890 Offers', imgSrc: '/Icons/brazil 1.svg' },
+        { name: 'India', offers: '3100 Offers', imgSrc: '/Icons/india 1.svg' },
+        { name: 'Colombia', offers: '150 Offers', imgSrc: '/Icons/colombia 1.svg' },
+        { name: 'Malaysia', offers: '2480 Offers', imgSrc: '/Icons/Frame 54.svg' },
+    ];
+
+    return (
+        // <div className='py-8'>
+        //     <div className='max-w-[1200px] mx-auto px-6'>
+        //         <h2 className='text-4xl font-bold text-customcol mb-7 pb-1'>Popular Countries</h2>
+        //         <div className='flex flex-wrap'>
+        //             <div className=''>
+        //                 <div className='pl-7 border-l-2 border-l-customColor'>
+        //                     <div className=' mb-6'>
+        //                         <img src='\Icons\france 1.svg' />
+        //                     </div>
+        //                     <div>
+        //                         <h4 className='mb-1 text-2xl text-customcol font-bold'>France</h4>
+        //                     </div>
+        //                     <p className='mb-1 text-base font-normal  text-lablecolor'>1500 Offers</p>
+
+        //                 </div>
+
+        //             </div>
+
+        //         </div>
+        //     </div>
+
+        // </div>
+
+        <div className='lg:py-14  py-8'>
+            <div className='max-w-[1200px] mx-auto px-6'>
+                <h2 className='text-4xl font-bold text-customcol mb-7 pb-1'>Popular Countries</h2>
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-12'>
+                    {countries.map((country, index) => (
+                        <div key={index} className='pl-7 border-l-2 border-l-customColor country-entry'>
+
+                            <div className='mb-6'>
+                                <img src={country.imgSrc} alt={country.name} />
+                            </div>
+                            <div className='relative'>
+                                <h4 className='mb-1 text-2xl text-customcol font-bold country-name'>{country.name}</h4>
+                            </div>
+                            <p className='mb-1 text-base font-normal text-lablecolor'>{country.offers}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PopularCountries
+
+  
